b8c3f3658eda5ba83b13be24c63f8152016d87c3
mustafakarakus <karakusmustafa@gmail.com>
2018-09-10 17:46:26 +0300

    Initial Commit

---
+++ Backbase Forecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Backbase Forecast
+//
+//  Created by Admin on 10.09.2018.
+//  Copyright © 2018 Backbase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
